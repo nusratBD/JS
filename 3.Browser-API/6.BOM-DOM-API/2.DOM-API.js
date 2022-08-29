2cce667551ae0bb51560574190804f0a3432947b
nusratBD <nusrat.hurain15@gmail.com>
2022-08-29 12:51:32 +0600

    DOM API

---
+++ 3.Browser-API/6.BOM-DOM-API/2.DOM-API.js
@@ -0,0 +1,15 @@
+/* 
+DOM: Document Object Model. It works with browser not with the JS.
+Example: document.getElementById('test');
+It is called DOM(Document Object Model), because it is similar to object property access. 
+Example: 
+const self = {
+    name: 'Nusrat',
+    age: 25
+}
+access: self.name;
+*/
+
+/* 
+As DOM works only in browser, it is called browser API.
+*/
