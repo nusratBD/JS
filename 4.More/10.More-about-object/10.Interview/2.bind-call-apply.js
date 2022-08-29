27ab193512cefcba87c6db386662ed9aaa4dc388
nusratBD <nusrat.hurain15@gmail.com>
2022-08-29 17:45:09 +0600

    More about object

---
+++ 4.More/10.More-about-object/10.Interview/2.bind-call-apply.js
@@ -0,0 +1,4 @@
+//২. (এডভান্সড) bind, call, apply এর মধ্যে ডিফারেন্স কি 
+/* 
+syntax is different.
+*/
---
+++ 4.More/10.More-about-object/3.Keys-values-entries-delete-seal-freeze/2.entries.js
@@ -0,0 +1,16 @@
+const self = {
+    name: 'Nusrat Hurain',
+    age: 25,
+    profession: 'Software Engineer',
+    salary: 50000000
+}
+const entries = Object.entries(self);
+console.log(entries);
+/* 
+output: [
+  [ 'name', 'Nusrat Hurain' ],
+  [ 'age', 25 ],
+  [ 'profession', 'Software Engineer' ],
+  [ 'salary', 50000000 ]
+]
+*/
