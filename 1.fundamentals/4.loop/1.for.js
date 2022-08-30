bb594f53e62fc7dfe7508cf534f10a64b0d5bce9
nusratBD <nusrat.hurain15@gmail.com>
2022-08-30 15:33:31 +0600

    for loop updated

--- 1.fundamentals/4.loop/1.for.js
+++ 1.fundamentals/4.loop/1.for.js
@@ -1,3 +1,30 @@
 for(let i = 0; i<=10; i++){
     console.log(i);
 }
+/* 
+output:
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+*/
+
+const numbers = [2, 3, 4, 5, 6];
+for(let i = 0; i<=numbers.length; i++){
+    console.log(numbers[i]);
+}
+/* 
+output:
+2
+3
+4
+5
+6
+*/
