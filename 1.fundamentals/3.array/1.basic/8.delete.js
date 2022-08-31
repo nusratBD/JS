711a97b9ccf23cae729a456b54d71b78d0ed869f
nusratBD <nusrat.hurain15@gmail.com>
2022-08-31 22:44:18 +0600

    array delete added

--- 1.fundamentals/3.array/1.basic/8.delete.js
+++ 1.fundamentals/3.array/1.basic/8.delete.js
@@ -1,3 +1,9 @@
+/*When we delete any array element, the element is deleted but it holds the position with <empty item> although the index becomes undefined. */
 const numbers2 = [2, 3, 4, 5];
 delete numbers2[2];
-console.log(numbers2);
+console.log(numbers2);//output: [ 2, 3, <1 empty item>, 5 ]
+console.log(numbers2[2]);//output: undefined
+delete numbers2[3];
+console.log(numbers2);//output: [ 2, 3, <2 empty items> ]
+delete numbers2[0];
+console.log(numbers2);//output: [ <1 empty item>, 3, <2 empty items> ]
