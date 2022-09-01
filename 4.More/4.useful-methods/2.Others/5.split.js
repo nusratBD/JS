4169246cad1b7aee2ed5a589db5f0c50c4cb0f1d
nusratBD <nusrat.hurain15@gmail.com>
2022-09-02 00:08:29 +0600

    split()

--- 4.More/4.useful-methods/2.Others/5.split.js
+++ 4.More/4.useful-methods/2.Others/5.split.js
@@ -1,5 +1,36 @@
+/* 
+The split() method splits a string into an array of substrings.
+The split() method returns the new array.
+The split() method does not change the original string.
+If (" ") is used as separator, the string is split between words.
+*/
 let anthem = 'Amar Sonar Bangla Ami Tomai Valobasi';
-// let split = anthem.split(' ');
-// let split = anthem.split('a');
-let split = anthem.split('a', 2);
+let split = anthem.split(' ');
 console.log(split);
+let split1 = anthem.split('a');
+/* 
+output:
+[
+  'Am',       'r Son',
+  'r B',      'ngl',
+  ' Ami Tom', 'i V',
+  'lob',      'si'
+]
+*/
+console.log(split1);
+let split2 = anthem.split('a', 2);//(a,2)=>a:split marker, 2=split numbers
+console.log(split2)//output: [ 'Am', 'r Son' ]
+
+let split3 = anthem.split('');
+console.log(split3);
+/* 
+output:
+[
+  'A', 'm', 'a', 'r', ' ', 'S', 'o',
+  'n', 'a', 'r', ' ', 'B', 'a', 'n',
+  'g', 'l', 'a', ' ', 'A', 'm', 'i',
+  ' ', 'T', 'o', 'm', 'a', 'i', ' ',
+  'V', 'a', 'l', 'o', 'b', 'a', 's',
+  'i'
+]
+*/
