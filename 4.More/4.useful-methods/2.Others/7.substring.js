49105a445d358181e4eda128656712cfe56780ac
nusratBD <nusrat.hurain15@gmail.com>
2022-09-02 00:13:09 +0600

    substring()

--- 4.More/4.useful-methods/2.Others/7.substring.js
+++ 4.More/4.useful-methods/2.Others/7.substring.js
@@ -1,3 +1,10 @@
+/* 
+The substring() method extracts characters, between two indices (positions), from a string, and returns the substring.
+The substring() method extracts characters from start to end (exclusive).
+The substring() method does not change the original string.
+If start is greater than end, arguments are swapped: (4, 1) = (1, 4).
+Start or end values less than 0, are treated as 0.
+*/
 let anthem = 'Amar Sonar Bangla Ami Tomai Valobasi';
 let subString = anthem.substring(5, 10);//(x,y)=>x=starting point:(x-1), y=ending point
-console.log(subString);
+console.log(subString);//output: Sonar
