2a8a65fdff0aa125be23079fdef1d4bd93e2e89e
nusratBD <nusrat.hurain15@gmail.com>
2022-09-02 00:10:35 +0600

    substr()

--- 4.More/4.useful-methods/2.Others/6.substr.js
+++ 4.More/4.useful-methods/2.Others/6.substr.js
@@ -1,5 +1,9 @@
-
-
+/* 
+The substr() method extracts a part of a string.
+The substr() method begins at a specified position, and returns a specified number of characters.
+The substr() method does not change the original string.
+To extract characters from the end of the string, use a negative start position.
+*/
 let anthem = 'Amar Sonar Bangla Ami Tomai Valobasi';
 let subStr = anthem.substr(5, 5);//(x, y)->x is starting point, y->number of character
-console.log(subStr);
+console.log(subStr);//output: Sonar
