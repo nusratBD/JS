dbbed820d1a57852766adc85b18a4280a54935b6
nusratBD <nusrat.hurain15@gmail.com>
2022-09-02 00:17:04 +0600

    toLowerCase() toUpperCase()

--- 4.More/4.useful-methods/2.Others/8.toLowerCase-toUpperCase.js
+++ 4.More/4.useful-methods/2.Others/8.toLowerCase-toUpperCase.js
@@ -1,6 +1,13 @@
+/* 
+The toLowerCase() method converts a string to lowercase letters.
+The toLowerCase() method does not change the original string. 
+
+The toUpperCase() method converts a string to uppercase letters.
+The toUpperCase() method does not change the original string.
+*/
 let name = 'Nusrat Hurain';
-// console.log(name.toLowerCase());
-// console.log(name.toUpperCase());
+console.log(name.toLowerCase());//output: nusrat hurain
+console.log(name.toUpperCase());//output: NUSRAT HURAIN
 let name1 = 'nusrat Hurain';
 let name2 = 'Nusrat hurain';
 if(name1 == name2){
@@ -9,3 +16,4 @@ if(name1 == name2){
 else if(name1.toLowerCase()==name2.toLowerCase()){
     console.log('They Matched by toLowerCase function');
 }
+//output: They Matched by toLowerCase function
