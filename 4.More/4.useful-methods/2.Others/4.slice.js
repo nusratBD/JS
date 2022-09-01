7a04c72efc0a66167297b9b3e15d19c9d85c0c72
nusratBD <nusrat.hurain15@gmail.com>
2022-09-02 00:14:20 +0600

    slice()

--- 4.More/4.useful-methods/2.Others/4.slice.js
+++ 4.More/4.useful-methods/2.Others/4.slice.js
@@ -1,7 +1,10 @@
 /* 
-The slice() method returns selected elements in an array, as a new array.
-The slice() method selects from a given start, up to a (not inclusive) given end.
-The slice() method does not change the original array.
+The slice() method extracts a part of a string.
+The slice() method returns the extracted part in a new string.
+The slice() method does not change the original string.
+The start and end parameters specifies the part of the string to extract.
+The first position is 0, the second is 1, ...
+A negative number selects from the end of the string.
 */
 //Examples:
 //01
