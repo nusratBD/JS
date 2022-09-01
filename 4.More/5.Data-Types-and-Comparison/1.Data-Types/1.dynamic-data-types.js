05c073f0d4f21ae4dfff9860cb18e5f16a301956
nusratBD <nusrat.hurain15@gmail.com>
2022-09-02 00:23:30 +0600

    dynamic data types

--- 4.More/5.Data-Types-and-Comparison/1.Data-Types/1.dynamic-data-types.js
+++ 4.More/5.Data-Types-and-Comparison/1.Data-Types/1.dynamic-data-types.js
@@ -2,21 +2,23 @@
 It also means that a dynamic type does not have a predefined type and can be used to store any type of data. JS is a dynamicaly typed variable. 
 Because its type defines on its rigth side's value. */
 
-// const a = 25;
-// const a = 'Nusrat Hurain';
-const a = true;
-console.log(typeof a);
+const a1 = 25;
+console.log(typeof a1);//output: number
+const a2 = 'Nusrat Hurain';
+console.log(typeof a2);//output: string
+const a3 = true;
+console.log(typeof a3);//output: boolean
 const numbers = [2, 3, 4, 5, 6];
 /* 
 1. Except primitive data type all are a kind of object in js, 
 such as- array, function etc
-2. type of null is object although it a primitive data.
+2. type of null is object although it is a primitive data.
 */
 console.log(typeof numbers); //output: object. array is a type of object in js
 function triple(x, y, z){
 
 }
-console.log(triple); //output: Function
-console.log(triple.length);//output: 3
+console.log(triple); //output: [Function: triple]
+console.log(triple.length);//output: 3=>(As like array length. Array is an object. So, function is also object)
 const x = null;
-console.log(typeof x);
+console.log(typeof x);//output:object
