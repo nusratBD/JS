dda1d3817ae90fe0dcffbbb6d6f73bb009a96b33
nusratBD <nusrat.hurain15@gmail.com>
2022-09-01 23:13:55 +0600

    concat and join method

--- 4.More/4.useful-methods/2.Others/1.concat.js
+++ 4.More/4.useful-methods/2.Others/1.concat.js
@@ -4,4 +4,22 @@ let lName = 'Nuha';
 
 // let fullName = fName + ' ' + mName + ' ' + lName;
 let fullName = fName.concat(' ').concat( mName).concat(' ').concat(lName);
-console.log(fullName); 
+// console.log(fullName); 
+
+//concat array:
+const arr1 = ["Cecilie", "Lone"];
+const arr2 = ["Emil", "Tobias", "Linus"];
+const children = arr1.concat(arr2);
+console.log(children);//output: [ 'Cecilie', 'Lone', 'Emil', 'Tobias', 'Linus' ]
+
+const arr11 = ["Cecilie", "Lone"];
+const arr22 = ["Emil", "Tobias", "Linus"];
+const arr33 = ["Robin"];
+const children1 = arr1.concat(arr22, arr33);
+console.log(children1);//output: [ 'Cecilie', 'Lone', 'Emil', 'Tobias', 'Linus', 'Robin' ]
+
+/* 
+The concat() method concatenates (joins) two or more arrays.
+The concat() method returns a new array, containing the joined arrays.
+The concat() method does not change the existing arrays.
+*/
--- 4.More/4.useful-methods/2.Others/3.join.js
+++ 4.More/4.useful-methods/2.Others/3.join.js
@@ -1,7 +1,20 @@
+/* 
+The join() method returns an array as a string.
+The join() method does not change the original array.
+Any separator can be specified. The default is comma (,).
+*/
 let myName = ['Nusrat', 'Hurain', 'Nuha'];
-// let join = myName.join();
-// let join = myName.join('');
-// let join = myName.join(' ');
-// let join = myName.join('-');
-let join = myName.join(', ');
-console.log(join);
+let join1 = myName.join();
+console.log(join1);//output: Nusrat,Hurain,Nuha
+
+let join2 = myName.join('');
+console.log(join2);//output: NusratHurainNuha
+
+let join3 = myName.join(' ');
+console.log(join3);//output: Nusrat Hurain Nuha
+
+let join4 = myName.join('-');
+console.log(join4);//output: Nusrat-Hurain-Nuha
+
+let join5 = myName.join(', ');
+console.log(join5);//output: Nusrat, Hurain, Nuha
