a4e71c4f212ce449f78805d238ed4ef16c6c89b3
nusratBD <nusrat.hurain15@gmail.com>
2022-09-02 00:40:45 +0600

    callback function

--- 4.More/7.Callback-Function/1.callback-function.js
+++ 4.More/7.Callback-Function/1.callback-function.js
@@ -1,6 +1,6 @@
 /* 
-At the time of calling a function if we pass an annonymous function a
-s a parameter of the calling function, this annonymous function is called callback function.
+At the time of calling a function if we pass an annonymous function as a parameter 
+of the calling function, this annonymous function is called callback function.
 Example: document.getElementById('my-btn).addEventListener('click', function(){
 })
 */
@@ -9,16 +9,16 @@ function test(name,print){
 }
 test('Nusrat',function(name){
     console.log('Hi', name);
-})
+})//output: Hi Nusrat
 
 //Multiple Callback Function
 function test2(data, callback1, callback2){
-    console.log(data);
+    console.log(data);//output: Nusrat Hurain: Software Engineer
     callback1(data);
     callback2(data);
 }
 test2('Nusrat Hurain: Software Engineer', function(data){
-    console.log(data.toUpperCase())
+    console.log(data.toUpperCase())//output: NUSRAT HURAIN: SOFTWARE ENGINEER
 }, function(data){
-    console.log(data.toLowerCase())
+    console.log(data.toLowerCase())//output: nusrat hurain: software engineer
 })
