b0a49e08a4c0d6d2bd030bdb31db1e3fea03c412
nusratBD <nusrat.hurain15@gmail.com>
2022-09-02 10:36:41 +0600

    borrow method bind

--- 4.More/11.More-about-object/6.borrow-method-bind/borrow-method-bind.js
+++ 4.More/11.More-about-object/6.borrow-method-bind/borrow-method-bind.js
@@ -22,9 +22,25 @@ const student2 = {
     income: 8000,
     balance: 8000
 }
+/* 
+output:
+Household Cost:  500
+{ name: 'MD. Y', class: 12, income: 8000, balance: 7500 }
+*/
 //binding balanceCal property with student2 object
 const bind2 = student1.balanceCal.bind(student2);
 bind2(500);
 console.log(student2);
 student1.balanceCal(6000);
 console.log(student1);
+/* 
+output:
+Household Cost:  6000 . Sorry your balance is insufficient.
+{
+  name: 'MD. X',
+  class: 10,
+  income: 5000,
+  balance: 5000,
+  balanceCal: [Function: service]
+}
+*/
