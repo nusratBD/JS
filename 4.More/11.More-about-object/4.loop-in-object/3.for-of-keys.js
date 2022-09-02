68dfdbe0b634c6df2e90f4b736c1e160e7e002d8
nusratBD <nusrat.hurain15@gmail.com>
2022-09-02 10:27:27 +0600

    for-of-keys

--- 4.More/11.More-about-object/4.loop-in-object/3.for-of-keys.js
+++ 4.More/11.More-about-object/4.loop-in-object/3.for-of-keys.js
@@ -5,7 +5,14 @@ const self = {
     salary: 50000000
 }
 const keys = Object.keys(self);
-console.log(keys);
+console.log(keys);//output: [ 'name', 'age', 'profession', 'salary' ]
 for(const key of keys){
     console.log(key,': ',self[key]);
+    /* 
+    output:
+    name :  Nusrat Hurain
+    age :  25
+    profession :  Software Engineer
+    salary :  50000000
+    */
 }
