103f3619cc7a9bad05a6de078c2edc7a5a91865f
nusratBD <nusrat.hurain15@gmail.com>
2022-09-02 10:18:41 +0600

    object-method-property

--- 4.More/11.More-about-object/2.Object-Method-Property/object-property.js
+++ 4.More/11.More-about-object/2.Object-Method-Property/object-property.js
@@ -19,9 +19,33 @@ const student = {
 }
 
 //Accessing Object Property
-student.job();
+student.job();//output: Nusrat Hurain will get a job very soon, ingSaAllah.
 const currentAmount = student.donation(10000000, 10000);
-console.log(currentAmount);
+console.log(currentAmount);//output: 39990000
 const currentAmount2 = student.donation(5000000, 10000);
-console.log(currentAmount2);
+console.log(currentAmount2);//output: 34980000
 console.log(student);
+/* 
+output:
+{
+  id: 101,
+  name: 'Nusrat Hurain',
+  major: 'Software Engineering',
+  subjects: [
+    'math',
+    'physics',
+    'chemistry',
+    'english',
+    'programming fundamentals',
+    'economics'
+  ],
+  hobbies: {
+    indoor: [ 'book reading, cooking, gardening' ],
+    outdoor: 'travelling'
+  },
+  isHardWorker: true,
+  job: [Function: job],
+  income: 34980000,
+  donation: [Function: donation]
+}
+*/
