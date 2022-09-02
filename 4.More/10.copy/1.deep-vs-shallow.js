d4060085cd3686e64d931f2636b97c4fa9f44e71
nusratBD <nusrat.hurain15@gmail.com>
2022-09-02 08:45:53 +0600

    deep copy vs shallow copy

---
+++ 4.More/10.copy/1.deep-vs-shallow.js
@@ -0,0 +1,3 @@
+/* 
+A deep copy means that all of the values of the new variable are copied and disconnected from the original variable. A shallow copy means that certain (sub-)values are still connected to the original variable.
+*/
