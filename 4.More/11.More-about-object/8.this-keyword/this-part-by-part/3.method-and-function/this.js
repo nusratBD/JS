51afb4a1a1ab8244ea3e4dcdb995d96e74f56065
nusratBD <nusrat.hurain15@gmail.com>
2022-09-02 10:42:08 +0600

    this in method and function

--- 4.More/11.More-about-object/8.this-keyword/this-part-by-part/3.method-and-function/this.js
+++ 4.More/11.More-about-object/8.this-keyword/this-part-by-part/3.method-and-function/this.js
@@ -7,7 +7,7 @@ const obj = {
     study: function(){
         console.log(this.name,': ',this.age);//output: Nusrat :  25
         function versity(){
-            console.log(this.name, 'is studying in self university');//   is studying in self university
+            console.log(this.name, 'is studying in self university');//undefined is studying in self university=>because varsity is a function not a method
         }
         versity();
     }
